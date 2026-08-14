18a4e83bf4c636a60132e44395ab9b5a8991e6c0
florinajugrastan-source <florina.jugrastan@.com>
2026-08-14 09:40:21 -0400

    final version

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -4,7 +4,8 @@ const resultDiv = document.getElementById('report');
 
 function clearSearch() {
     document.getElementById("conditionInput").value = "";
-    document.getElementById('report').innerHTML = "";
+    document.getElementById('report').innerHTML = ""
+    document.getElementById('searchResults').style.visibility = 'hidden';
 }
 
 function searchDest() {
@@ -15,41 +16,50 @@ function searchDest() {
         .then(response => response.json())
         .then(data => {
             switch (input){
+                case "temples":
                 case "temple":{
                     const dest_array = data.temples;
                     generateDestinations(dest_array);
                 }
                 break;
+                case "beaches":
                 case "beach":{
                     const dest_array = data.beaches;
                     generateDestinations(dest_array);
                 }
                 break;
+                case "countries":
+                case "country":{
+                    const dest_array = data.countries;
+                    const nr = Math.floor(Math.random() * 4);
+                    const country = dest_array.find(item => item.id == nr);
+                    generateDestinations(country.cities);
+                }
+                break;
                 default:{
                     const dest_array = data.countries;
                     const country = dest_array.find(item => item.name.toLowerCase() === input);
-                    console.log(country);
                     generateDestinations(country.cities);
                 }
             }
         })
         .catch(error => {
             console.error('Error:', error);
-            resultDiv.innerHTML = 'An error occurred while fetching data.';
+            resultDiv.innerHTML = '<div class="resitem">An error occurred while fetching data.</div>';
         });
 }
 
 function generateDestinations(dest_array){
-    resultDiv.innerHTML = "<h2>Search results</h2><br>";
+    resultDiv.innerHTML = "";
     let divcontent = '';
 
     for (let i=0; i<dest_array.length; i++) {
-        divcontent += `<img src="${dest_array[i].imageUrl}">`;
+        divcontent = `<img src="${dest_array[i].imageUrl}">`;
         divcontent += `<p><strong>${dest_array[i].name}</strong></p>`;
         divcontent += `<p>${dest_array[i].description}</p>`;
-        resultDiv.innerHTML += `<div class="resitem">${divcontent}</div>`;
+        resultDiv.innerHTML += `<div class="resitem">${divcontent}</div><br>`;
     }
-
+    document.getElementById('searchResults').style.visibility = 'visible';
 }
 
 btnSearch.addEventListener('click', searchDest);
